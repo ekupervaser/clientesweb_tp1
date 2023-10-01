c065a24da89030360a8a63f9634f64aa3f542317
Ezequiel Kupervaser <ezequiel@kyas.com.ar>
2023-10-01 19:07:32 -0300

    mejoras en componentes header y footer

--- src/app.vue
+++ src/app.vue
@@ -2,11 +2,12 @@
 import Chat from './pages/Chat.vue';
 import { logout, subscribeToAuth } from './services/Auth'
 import Home from './pages/Home.vue';
+import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
 
 export default {
     name: "App",
-    components: { Chat, Home, Footer },
+    components: { Chat, Home, Header, Footer },
     data() {
         return {
             user: {
@@ -30,52 +31,14 @@ export default {
 
 <template>
 
-    <header class="flex gap-4 items-center p-4 bg-blue-50">
-        <div>
-            <img src="../favicon-32x32.png" alt="">
-        </div>
-        <nav>
-            <ul class="flex gap-5">
-                <li>
-                    <router-link to="/">Home</router-link>
-                </li>
-                <li>
-                    <router-link to="/quienes-somos">About</router-link>
-                </li>
-                <template v-if="user.id === null">
-                    <li>
-                        <router-link to="/registro">Registro</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/login">Iniciar sesión</router-link>
-                    </li>
-                </template>
-                <template v-else>
-                    <li>
-                        <router-link to="/chat">Chat</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/perfil">Mi perfil</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/panel">Panel</router-link>
-                    </li>
-                    <li>
-                        <form action=""
-                        @submit.prevent="handleLogout"
-                        >
-                            <button type="submit">{{user.email}} (Cerrar sesión)</button>
-                        </form>
-                    </li>
-                </template>
-            </ul>
-        </nav>
-    </header>
+    <Header></Header>
 
     <div class="container m-auto p-10">
 
      <router-view></router-view>
+
     </div>
 
-<Footer></Footer>
+    <Footer></Footer>
+
 </template>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,16 +5,18 @@ export default {
 </script>
 
 <template>
-     <footer class="container bg-blue-700 text-white h-20 flex justify-center">
-        <div class="flex justify-between items-center p-4 w-full">
-            <div>
-                <p>Clientes Web Mobile</p>
-                <p>Trazajo Práctico N° 1</p>
-            </div>
-            <div>
-                <p>Ezequiel Kupervaser</p>
-                <p>María Belén Zárate</p>
-            </div>
+     <footer class="bg-blue-700 text-white h-20 flex justify-center">
+        <div class="container m-auto">
+            <div class="flex justify-between items-center p-4 w-full">
+                <div>
+                    <p>Clientes Web Mobile</p>
+                    <p>Trazajo Práctico N° 1</p>
+                </div>
+                <div>
+                    <p>Ezequiel Kupervaser</p>
+                    <p>María Belén Zárate</p>
+                </div>
         </div>
+    </div>
     </footer>
 </template>
